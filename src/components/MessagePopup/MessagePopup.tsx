da908efc477166ccd7ec178d13e2ec415bd32c8e
Nicholas Chumney <nicholas.chumney@outlook.com>
2022-03-23 19:33:08 -0400

    added connection for message popup

--- src/components/MessagePopup/MessagePopup.tsx
+++ src/components/MessagePopup/MessagePopup.tsx
@@ -1,10 +1,25 @@
+import { useSelector } from 'react-redux';
+
+import { IRootState } from '../../helpers/reducers';
 import styles from './MessagePopup.module.css';
 
 const MessagePop = () => {
+  const { isRunning, gameOver } = useSelector((state: IRootState) => state.game);
+
+  let message = '';
+  let isHidden = true;
+
+  if (gameOver) {
+    message = 'Game Over';
+    isHidden = false;
+  } else if (!isRunning) {
+    message = 'Paused';
+    isHidden = false;
+  }
+
   return (
-    <div className={styles.MessagePop}>
-      <h1>Message Title</h1>
-      <p>Message info...</p>
+    <div className={`${styles.MessagePopup} ${isHidden ? styles.Hidden : ''}`}>
+      <h1>{message}</h1>
     </div>
   );
 }
