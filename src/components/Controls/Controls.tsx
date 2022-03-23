4791d6bcc66ebadc07c3b526d8cfef46478b944b
Nicholas Chumney <nicholas.chumney@outlook.com>
2022-03-22 20:18:18 -0400

    added connection for controls

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,14 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { moveDown, moveLeft, moveRight, rotate } from '../../helpers/actions';
+import { IRootState } from '../../helpers/reducers';
 import styles from './Controls.module.css';
 
 const Controls = () => {
-  const handleClick = () => {}
+  const dispatch = useDispatch();
+  const isRunning = useSelector((state: IRootState) => state.game.isRunning);
+
+  const handleLeft = () => {
+    dispatch(moveLeft());
+  }
+
+  const handleRight = () => {
+    dispatch(moveRight());
+  }
+
+  const handleDown = () => {
+    dispatch(moveDown());
+  }
+
+  const handleRotate = () => {
+    dispatch(rotate());
+  }
 
   return (
     <div className={styles.Controls}>
-      <button className={styles.Button} onClick={handleClick}>Left</button>
-      <button className={styles.Button} onClick={handleClick}>Right</button>
-      <button className={styles.Button} onClick={handleClick}>Rotate</button>
-      <button className={styles.Button} onClick={handleClick}>Down</button>
+      <button className={styles.Button} onClick={handleLeft}>Left</button>
+      <button className={styles.Button} onClick={handleRight}>Right</button>
+      <button className={styles.Button} onClick={handleDown}>Rotate</button>
+      <button className={styles.Button} onClick={handleRotate}>Down</button>
     </div>
   );
 }
