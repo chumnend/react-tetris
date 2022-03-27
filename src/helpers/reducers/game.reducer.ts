85fc9b06d394aec25189a6ae22e2db42433901b7
Nicholas Chumney <nicholas.chumney@outlook.com>
2022-03-27 11:25:09 -0400

    move left and right

--- src/helpers/reducers/game.reducer.ts
+++ src/helpers/reducers/game.reducer.ts
@@ -4,17 +4,24 @@ import { defaultState, nextRotation, canMoveTo} from '../utils';
 
 
 const gameReducer = (state = defaultState(), action: ActionType) => {
+  const {shape, grid, x, y, rotation} = state;
+
   switch(action.type) {
     case ACTIONS.MOVE_LEFT:
+      if (canMoveTo(shape, grid, x - 1, y, rotation)) {
+        return { ...state, x: x - 1 };
+      }
       return state;
     case ACTIONS.MOVE_RIGHT:
+      if (canMoveTo(shape, grid, x + 1, y, rotation)) {
+        return { ...state, x: x + 1 };
+      }
       return state;
     case ACTIONS.MOVE_DOWN:
       return state;
     case ACTIONS.ROTATE:
-      const {shape, grid, x, y, rotation} = state;
       const newRotation = nextRotation(shape, rotation);
-      if(canMoveTo(shape, grid, x, y, newRotation)) {
+      if (canMoveTo(shape, grid, x, y, newRotation)) {
         return { ...state, rotation: newRotation };
       }
       return state;
